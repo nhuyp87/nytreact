2eafea25818f8496ac2d0795117962ee34addc5a
nhuyp <ypham87@gmail.com>
2017-08-01 20:50:02 -0700

    article model

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,35 +2,23 @@
 var React = require("react");
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
-
-// Create the Main component
 var Main = React.createClass({
-
-    // Here we render the component
-    render: function () {
-
-        return (
-            <div className="container">
-
-                <div className="jumbotron">
-                    <h1>NYT Article Scrubber</h1>
-                    <p><em>Search and annotate articles of interest.</em></p>
-                    <Link to="/info"><button className="btn btn-default">Info</button></Link>
-                </div>
-
-
-                <div className="row">
-
-                    {/* Added this.props.children to dump all of the child components into place */}
-                    {this.props.children}
-
-                </div>
-
-
-            </div> // Closes container
-        );
-    }
+  // Here we render the function
+  render: function() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h2><strong>NYT Article Scrubber</strong></h2>
+          <p><em>Search and annotate articles of interest.</em></p>
+          <hr />
+        </div>
+        <div className="row">
+          {/* This code will dump the correct Child Component */}
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 });
-
 // Export the component back for use in other files
 module.exports = Main;
